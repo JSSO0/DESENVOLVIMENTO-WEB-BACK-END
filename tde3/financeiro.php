232a8deef5b2f50822b30933f7c528f8ae981678
Joely <sjoely131@gmail.com>
2023-05-16 23:28:39 -0300

    financeiro ok

---
+++ tde3/financeiro.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SITE GATO NET</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/stylefinanceiro.css">
+
+</head>
+
+<body>
+
+    <header class="header">
+
+        <a href="#" class="logo">
+            <img src="images/logo.png" alt="">
+        </a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#planos">planos</a>
+            <a href="#projetos">Produtos</a>
+            <a href="#review">review</a>
+            <a href="#contato">contato</a>
+            <a href="#blogs">Links Úteis</a>
+        </nav>
+
+        <div class="icons">
+            <div class="fas fa-search" id="search-btn"></div>
+            <div class="fas fa-shopping-cart" id="cart-btn"></div>
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+
+        <div class="search-form">
+            <input type="search" id="search-box" placeholder="search here...">
+            <label for="search-box" class="fas fa-search"></label>
+        </div>
+
+
+    </header>
+
+    <section class="home" id="home">
+
+        <div class="content">
+            <h3>GATO NET</h3>
+            <p>PLANOS MAIS BARATOS DO BRASIL</p>
+            <a href="#planos" class="btn">Conheça nossos planos</a>
+        </div>
+
+    </section>
+
+    <section class="planos" id="planos">
+
+        <h1 class="heading"> <span>planos</span></h1>
+
+        <div class="row">
+
+            <div class="image">
+                <img src="images/about-img.jpeg" alt="">
+            </div>
+
+            <div class="content">
+                <h3>Nossos planos!</h3>
+                <p>ESCOLHA UM DE NOSSOS PLANOS E SINTA O QUE HÁ DE MELHOR EM TECNOLOGIAS COMO INTERNET SEM FIO, 68
+                    CANAIS LIBERADOS E UM FIXO PARA RECEBER COBRANÇAS</p>
+                <a href="#projetos" class="btn">Contrate abaixo</a>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section class="projetos" id="projetos">
+
+
+        <h1 class="heading"> Confira <span>Abaixo</span> </h1>
+
+        <div class="box-container">
+
+            <div class="box" id="caixaplano">
+                
+                <div class="icons">
+                    <!--  <a href="#" class="fas fa-shopping-cart"></a>  -->
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <div class="image">
+                    <a href="cadastro.php"><img src="images/20mb.svg" alt=""></a>
+                </div>
+                <div class="content">
+                    <h3>Wifi + TV E Plano Básico</h3>
+
+                    <div class="price">R$55,00 <span>R$69,90</span></div>
+                </div>
+
+            </div>
+
+            <div class="box" id="caixaplano">
+                <div class="icons">
+                    <!--  <a href="#" class="fas fa-shopping-cart"></a>  -->
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <div class="image">
+                <a href="cadastro.php"> <img src="images/50mb.svg" alt=""></a>
+                </div>
+                <div class="content">
+                    <h3>Wifi + Tv + Fixo E Plano Básico</h3>
+
+                    <div class="price">R$75,00 <span>R$89,99</span></div>
+                </div>
+            </div>
+
+            <div class="box" id="caixaplano">
+                <div class="icons">
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <div class="image">
+                <a href="cadastro.php"> <img src="images/150mb.svg" alt=""></a>
+                </div>
+                <div class="content">
+                    <h3>Wifi + Tv + Fixo E Plano intermediário</h3>
+
+                    <div class="price">R$109,00 <span>R$159,99</span></div>
+                </div>
+            </div>
+
+        </div>
+        <br>
+        <div class="box-container">
+
+            <div class="box" id="caixaplano">
+                <div class="icons">
+                    <!--  <a href="#" class="fas fa-shopping-cart"></a>  -->
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <div class="image">
+                <a href="cadastro.php"><img src="images/400mb.svg" alt=""></a>
+                </div>
+                <div class="content">
+                    <h3>Wifi + TV E Plano Avançado</h3>
+
+                    <div class="price">R$149,00 <span>R$189,90</span></div>
+                </div>
+            </div>
+
+            <div class="box" id="caixaplano">
+                <div class="icons">
+                    <!--  <a href="#" class="fas fa-shopping-cart"></a>  -->
+                    <a href="#" class="fas fa-heart"></a>
+                    <a href="#" class="fas fa-eye"></a>
+                </div>
+                <div class="image">
+                <a href="cadastro.php"> <img src="images/600mb.svg" alt=""></a>
+                </div>
+                <div class="content">
+                    <h3>Wifi + Tv + Fixo E Plano Avançado</h3>
+
+                    <div class="price">R$199,00 <span>R$289,99</span></div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="review" id="review">
+
+        <h1 class="heading"> NOSSOS <span>CLIENTES</span> </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="images/quote-img.png" alt="" class="quote">
+                <p>Não sou capaz de opinar</p>
+                <a href="cadastro.php"><img src="images/gloria.png" class="user" alt=""></a>
+                <h3>Gloria Pirez</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="images/quote-img.png" alt="" class="quote">
+                <p> Um pouco mais</p>
+                <a href="cadastro.php"><img src="images/leonidas.png" class="user" alt=""></a>
+                <h3>Leonidas de Esparta</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="images/quote-img.png" alt="" class="quote">
+                <p>Uau</p>
+                <a href="cadastro.php"> <img src="images/jacke.png" class="user" alt=""></a>
+                <h3>Jackie Chan</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <section class="contato" id="contato">
+
+    <h1 class="heading"> <span>JÁ É CLIENTE?</span> ACESSE! </h1>
+
+    <div class="row">
+
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.3031299674503!2d-39.31377462584139!3d-7.206216792799318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a178ad71080c59%3A0x5b2a22386c2711fd!2sCentro%20Universit%C3%A1rio%20Para%C3%ADso%20-%20UniFAP!5e0!3m2!1spt-BR!2sbr!4v1683835516014!5m2!1spt-BR!2sbr" allowfullscreen="" loading="lazy" ></iframe>
+
+        <form action="">
+            <h3>PREENCHA SEUS DADOS</h3>
+                <div class="inputBox">
+                <span class="fas fa-envelope"></span>
+                <input type="email" placeholder="EMAIL">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-key"></span>
+                <input type="password" placeholder="SENHA">
+            </div>
+            <input type="submit" value="Entrar" class="btn">
+            <a href="cadastro.php" class="btn">Clique Aqui Caso não tenha Cadastro</a>
+        </form>
+
+    </div>
+
+</section>
+
+
+
+    <section class="blogs" id="blogs">
+
+        <h1 class="heading"> Links <span>Úteis</span> </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/blog-1.jpeg" alt="">
+                </div>
+                <div class="content">
+                    <a href="#" class="title">Suporte Técnico</a>
+                    <span>Solicite aqui o Suporte</span>
+                    <p>Solicite aqui o suporte ou reinicie o roteador para tentar sozinho!</p>
+                    <button><a href="suporte.php" class="btn">Suporte</a> </a></button>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/blog-2.jpeg" alt="">
+                </div>
+                <div class="content">
+                    <a href="#" class="title">Financeiro</a>
+                    <span>Acesse suas faturas</span>
+                    <p>Tenha o controle financeiro e não nos faça perder tempo cancelando seus beneficios!</p>
+                    <button><a href="financeiro.php" class="btn">Financeiro</a> </a></button>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/blog-3.jpeg" alt="">
+                </div>
+                <div class="content">
+                    <a href="#" class="title">tasty and refreshing coffee</a>
+                    <span>by admin / 21st may, 2021</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+                    <a href="#" class="btn">read more</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <section class="footer">
+
+        <div class="share">
+            <a href="https://www.facebook.com/eduardo.rocha.3557/" class="fab fa-facebook-f"></a>
+            <a href="https://www.linkedin.com/in/eduardo-sampaio-05b4ab247/" class="fab fa-github"></a>
+            <a href="https://www.instagram.com/du_sampaio/" class="fab fa-instagram"></a>
+            <a href="https://www.linkedin.com/in/eduardo-sampaio-05b4ab247/" class="fab fa-linkedin"></a>
+        </div>
+
+        <div class="links">
+            <a href="#home">home</a>
+            <a href="#planos">planos</a>
+            <a href="#projetos">Produtos</a>
+            <a href="#review">review</a>
+            <a href="#contato">contato</a>
+            <a href="#blogs">Links Úteis</a>
+        </div>
+
+        <div class="credit">created by <span>Eduardo Sampaio</span> | all rights reserved</div>
+
+    </section>
+
+
+
+
+
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
